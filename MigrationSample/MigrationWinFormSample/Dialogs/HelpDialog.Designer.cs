5de7009e51dfc7ddc9bdb707555bc482bbe40957
Gregory Borodin <gregbor@microsoft.com>
2017-06-19 02:23:25 -0700

    Add Help Dialog

---
+++ MigrationSample/MigrationWinFormSample/Dialogs/HelpDialog.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace MigrationWinFormSample.Dialogs
+{
+    partial class HelpDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.InstractionsLbl = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.HelpBtn = new System.Windows.Forms.Button();
+            this.OKbtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.InstractionsLbl, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 59.48276F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40.51724F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(578, 116);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // InstractionsLbl
+            // 
+            this.InstractionsLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InstractionsLbl.AutoSize = true;
+            this.InstractionsLbl.BackColor = System.Drawing.SystemColors.Window;
+            this.InstractionsLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InstractionsLbl.Location = new System.Drawing.Point(3, 0);
+            this.InstractionsLbl.Name = "InstractionsLbl";
+            this.InstractionsLbl.Size = new System.Drawing.Size(572, 69);
+            this.InstractionsLbl.TabIndex = 0;
+            this.InstractionsLbl.Text = "label1";
+            this.InstractionsLbl.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.HelpBtn);
+            this.flowLayoutPanel1.Controls.Add(this.OKbtn);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(7, 76);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(7);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(564, 33);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // HelpBtn
+            // 
+            this.HelpBtn.Location = new System.Drawing.Point(465, 3);
+            this.HelpBtn.Name = "HelpBtn";
+            this.HelpBtn.Size = new System.Drawing.Size(96, 26);
+            this.HelpBtn.TabIndex = 0;
+            this.HelpBtn.Text = "Help";
+            this.HelpBtn.UseVisualStyleBackColor = true;
+            this.HelpBtn.Click += new System.EventHandler(this.HelpBtn_Click);
+            // 
+            // OKbtn
+            // 
+            this.OKbtn.Location = new System.Drawing.Point(363, 3);
+            this.OKbtn.Name = "OKbtn";
+            this.OKbtn.Size = new System.Drawing.Size(96, 26);
+            this.OKbtn.TabIndex = 1;
+            this.OKbtn.Text = "OK";
+            this.OKbtn.UseVisualStyleBackColor = true;
+            this.OKbtn.Click += new System.EventHandler(this.OKbtn_Click);
+            // 
+            // HelpDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 116);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "HelpDialog";
+            this.Text = "HelpDialog";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label InstractionsLbl;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button HelpBtn;
+        private System.Windows.Forms.Button OKbtn;
+    }
+}
---
+++ MigrationSample/MigrationWinFormSample/Dialogs/HelpDialog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MigrationWinFormSample.Dialogs
+{
+
+    public partial class HelpDialog : Form
+    {
+        private string HelpUrl { get; set; }
+
+        public HelpDialog(string title, string text, string helpUrl)
+        {
+            InitializeComponent();
+
+            InstractionsLbl.Text = text;
+
+            this.Text = title;
+
+            HelpUrl = helpUrl;
+        }
+
+        private void HelpBtn_Click(object sender, EventArgs e)
+        {
+            Process.Start(HelpUrl);
+        }
+
+        private void OKbtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
